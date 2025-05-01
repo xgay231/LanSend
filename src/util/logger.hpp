574c77d5416d24adbde1c3c40c5b7901d5ccf9cf
xgay231 <xgay231@qq.com>
2025-05-02 00:06:17 +0800

    feat(logger): implement basic logging functionality

--- src/util/logger.hpp
+++ src/util/logger.hpp
@@ -1,3 +1,15 @@
 #pragma once
+#include <string>
 
-class Logger {};
+class Logger {
+public:
+    static Logger& get_instance();
+
+    // 添加以下方法以解决 "info" 成员缺失的问题
+    void info(const std::string& message);
+    void warn(const std::string& message);
+    void error(const std::string& message);
+
+private:
+    Logger() = default;
+};
