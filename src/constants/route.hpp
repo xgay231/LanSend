330cdf1b5c24daba7189ff1cbab5a64f65a01395
Mairon1206 <3043462073@qq.com>
2025-05-16 01:45:31 +0800

    feat(route): add `ApiRoute`with predefined API endpoints

---
+++ src/constants/route.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string_view>
+
+namespace lansend {
+
+class ApiRoute {
+public:
+    static constexpr std::string_view kInfo = "/info";
+    static constexpr std::string_view kSendRequest = "/send-request";
+    static constexpr std::string_view kPrepareSend = "/prepare-send";
+    static constexpr std::string_view kSendChunk = "/send-chunk";
+    static constexpr std::string_view kVerifyAndComplete = "/verify-and-complete";
+    static constexpr std::string_view kCancelSend = "/cancel-send";
+    static constexpr std::string_view kCancelReceive = "/cancel-receive";
+};
+
+} // namespace lansend
